3e2fd9147fba046f38ca2db1de58f8ba159feb28
ThiagoDiasV <thiago76ers@gmail.com>
2020-05-12 07:32:01 -0300

    fix: celery bug

--- brazil/tasks.py
+++ brazil/tasks.py
@@ -7,7 +7,7 @@ logger = get_task_logger(__name__)
 
 
 @periodic_task(
-    run_every=(crontab(minute="*/10")),
+    run_every=(crontab(minute="*/60")),
     name="search_for_empty_data_to_save",
     ignore_result=True,
 )
