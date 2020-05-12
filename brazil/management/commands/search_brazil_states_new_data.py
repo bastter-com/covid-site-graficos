e85d05d9cf4babbaf342e05023365549778d027b
ThiagoDiasV <thiago76ers@gmail.com>
2020-05-12 11:48:20 -0300

    feature: two new custom commands to save data to database

---
+++ brazil/management/commands/search_brazil_states_new_data.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+from brazil.services.save_data_states import search_for_empty_data_to_save
+
+
+class Command(BaseCommand):
+    help = 'Save new Brazil states data to database'
+
+    def handle(self, *args, **kwargs):
+        search_for_empty_data_to_save()
---
+++ world/management/commands/search_world_new_data.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from world.services.save_total_world_data import search_for_empty_data_to_save_at_totals_data
+from world.services.save_country_daily_data import search_for_empty_data_to_save_at_country_data
+
+
+class Command(BaseCommand):
+    help = "Save new world data to database"
+
+    def handle(self, *args, **kwargs):
+        search_for_empty_data_to_save_at_totals_data()
+        search_for_empty_data_to_save_at_country_data()
