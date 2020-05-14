35c902b3cde887fa878bf04e371eecb5b4d1762a
ThiagoDiasV <thiago76ers@gmail.com>
2020-05-14 10:23:27 -0300

    fix: adding city model

--- city/models.py
+++ city/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from brazil.models import StateData
 
-# Create your models here.
+
+class CityData(models.Model):
+    state = models.CharField(max_length=3)
+    city = models.CharField(max_length=80)
+    city_ibge_code = models.CharField(max_length=12)
+    confirmed = models.PositiveIntegerField()
+    confirmed_per_100k_inhabitants = models.FloatField()
+    date = models.DateField()
+    death_rate = models.FloatField()
+    deaths = models.PositiveIntegerField()
+    estimated_population_2019 = models.PositiveIntegerField()
