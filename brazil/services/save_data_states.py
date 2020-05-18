8572b4c7bbdd2495833700eea659fb9e2abb3e2c
ThiagoDiasV <thiago76ers@gmail.com>
2020-05-18 01:39:51 -0300

    test: save to database delayed data

--- brazil/services/save_data_states.py
+++ brazil/services/save_data_states.py
@@ -62,7 +62,10 @@ def create_date_list():
             queryset = StateData.objects.filter(date=date)
             if len(queryset) == 27 and index != 0:
                 if index != 0:
-                    first_date = list_of_dates_to_check[index - 1]
+                    from ipdb import set_trace
+
+                    set_trace()
+                    first_date = list_of_dates_to_check[index + 3]
                     index = list_of_dates_to_check.index(first_date)
                     first_date = first_date.strftime("%Y-%m-%d")
                     list_of_dates_to_save_data = list_of_dates_to_check[
