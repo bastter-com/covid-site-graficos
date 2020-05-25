6bca31172cf81f6cbbd4f44134543303cc248a5a
ThiagoDiasV <thiago76ers@gmail.com>
2020-05-25 18:38:32 -0300

    fix: preparing to change from heroku to digital ocean server

--- core/settings.py
+++ core/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 
 LOGGING = {
