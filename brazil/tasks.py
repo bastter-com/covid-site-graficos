f339313e7adde83cd9136d3749cba615234b2cfc
ThiagoDiasV <thiago76ers@gmail.com>
2020-05-26 00:57:48 -0300

    test: celery remotely

--- brazil/tasks.py
+++ brazil/tasks.py
@@ -10,7 +10,7 @@ logger = get_task_logger(__name__)
 
 
 @periodic_task(
-    run_every=(crontab(minute="*/60")),
+    run_every=(crontab(minute="*/5")),
     name="search_for_empty_data_to_save",
     ignore_result=True,
 )
