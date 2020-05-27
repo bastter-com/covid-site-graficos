e5966bb2a310268dff0d39fb8dce3f2262693cc8
ThiagoDiasV <thiago76ers@gmail.com>
2020-05-27 12:36:17 -0300

    add: celery task to copy previous register when no register

--- brazil/services/update_data_using_ms_base.py
+++ brazil/services/update_data_using_ms_base.py
@@ -19,6 +19,8 @@ def instantiate_webdriver():
     print("Opening webdriver instance...")
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome("/usr/bin/chromedriver", options=chrome_options)
     return driver
 
