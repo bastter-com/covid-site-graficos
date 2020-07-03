a259eef197e9cfb62326685deef625bf1eaa5767
ThiagoDiasV <thiago76ers@gmail.com>
2020-07-03 13:17:52 -0300

    add: new celery task to save previous city data

--- brazil/tasks.py
+++ brazil/tasks.py
@@ -5,6 +5,7 @@ from django.core.management import call_command
 from brazil.services.save_data_states import (
     search_for_empty_data_to_save,
     search_for_empty_registers_between_two_dates_or_before_first_case,
+    save_previous_city_data_to_database,
 )
 from brazil.services.update_data_using_ms_base import (
     pipeline_to_save_data_using_ms_source,
@@ -51,6 +52,19 @@ def task_fix_empty_registers_copying_last_register():
     """
     search_for_empty_registers_between_two_dates_or_before_first_case()
     logger.info("Date parsed succesfully.")
+
+@periodic_task(
+    run_every=(crontab(minute=0, hour=1)),
+    name="save_previous_city_data_if_exists",
+    ignore_result=True,
+)
+def task_save_previous_city_data_if_exists():
+    """
+    Celery task to copy the previous register when no existing register
+    in some date.
+    """
+    save_previous_city_data_to_database()
+    logger.info("City data task done succesfully.")
 
 
 @periodic_task(
